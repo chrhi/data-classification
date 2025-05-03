f2b1af4dad2c99c49a5f0ccd22371c7a8e9cfe31
abdellah <Abdellah.chehri14@gmail.com>
2025-05-03 18:13:49 +0200

    feat : remove unused files

--- app/policy-generator/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type React from "react"
-export default function PolicyGeneratorLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <div className="flex-1 overflow-auto">{children}</div>
-}
