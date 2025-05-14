e6e1770e03f01f643946a202399f3103261c246f
abdellah <Abdellah.chehri14@gmail.com>
2025-05-14 07:46:47 +0200

    feat: update prisma lib/db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 // import { PrismaClient } from '../prisma/generated/clientPg'
 
 declare global {
